d9b3b449a46922585dff64072d92ed3be6474155
nicodev-git <schaefernicker@gmail.com>
2020-04-22 04:12:18 +0800

    Clean Code

--- src/client/components/Landing/Login/index.js
+++ src/client/components/Landing/Login/index.js
@@ -10,99 +10,99 @@ import {loginUser} from '../../../actions/authActions';
 import './Login.css'
 
 class NormalLoginForm extends React.Component {
-	handleSubmit = e => {
-		e.preventDefault();
-		this.props.form.validateFields((err, values) => {
-			if (!err) {
-				this.props.loginUser(values)
-			}
-		});
-	};
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.loginUser(values)
+      }
+    });
+  };
 
-	componentWillMount(){
-		 if(localStorage.getItem('token'))
-			 window.location = '/'
-	}
+  componentWillMount(){
+     if(localStorage.getItem('token'))
+       window.location = '/'
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.auth.isAuthenticated) {
-			this.props.history.push('/dashboard');
-		}
-		
-		if (nextProps.errors) {
-			this.setState({errors: nextProps.errors});
-		}
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+    
+    if (nextProps.errors) {
+      this.setState({errors: nextProps.errors});
+    }
+  }
 
-	render() {
-		const { getFieldDecorator } = this.props.form;
-	 
-		return (
-			<div className="login-screen p-3">
-				<div className="login-box">
-					<Row>
-						<Col md={12}>
-							<LandingIntro/>
-						</Col>
-						<Col md={12}>
-							<div className="p-4 pt-5 flex-1 pb-5">
-								<h2 className="text-center mb-4">Sign In</h2>
-								<Form onSubmit={this.handleSubmit} className="login-form">
-									<Form.Item hasFeedback>
-										{getFieldDecorator('email', {
-											rules: [{ required: true, message: 'Please input your email!' }],
-										})(
-											<Input
-												prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-												placeholder="Email"
-											/>,
-										)}
-									</Form.Item>
-									<Form.Item hasFeedback>
-										{getFieldDecorator('password', {
-											rules: [{ required: true, message: 'Please input your Password!' }],
-										})(
-											<Input
-												prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-												type="password"
-												placeholder="Password"
-											/>,
-										)}
-									</Form.Item>
-									<Form.Item className="col-8 ml-auto mr-auto mb-3">
-										<Button type="primary" htmlType="submit" className="login-form-button ">
-											Sign in
-										</Button>
-									</Form.Item>
-								</Form>
-								 <p className="mg-b-0 text-center">
-									Don't have an account? <Link to="/register">Sign Up</Link>
-								</p>
-							</div>
-						</Col>
-					</Row>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { getFieldDecorator } = this.props.form;
+   
+    return (
+      <div className="login-screen p-3">
+        <div className="login-box">
+          <Row>
+            <Col md={12}>
+              <LandingIntro/>
+            </Col>
+            <Col md={12}>
+              <div className="p-4 pt-5 flex-1 pb-5">
+                <h2 className="text-center mb-4">Sign In</h2>
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                  <Form.Item hasFeedback>
+                    {getFieldDecorator('email', {
+                      rules: [{ required: true, message: 'Please input your email!' }],
+                    })(
+                      <Input
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="Email"
+                      />,
+                    )}
+                  </Form.Item>
+                  <Form.Item hasFeedback>
+                    {getFieldDecorator('password', {
+                      rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                      <Input
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        type="password"
+                        placeholder="Password"
+                      />,
+                    )}
+                  </Form.Item>
+                  <Form.Item className="col-8 ml-auto mr-auto mb-3">
+                    <Button type="primary" htmlType="submit" className="login-form-button ">
+                      Sign in
+                    </Button>
+                  </Form.Item>
+                </Form>
+                 <p className="mg-b-0 text-center">
+                  Don't have an account? <Link to="/register">Sign Up</Link>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 const Login = Form.create({ name: 'normal_login' })(NormalLoginForm);
 
 const mapStateToProps = state => ({
-	auth: state.auth,
-	errors: state.errors
+  auth: state.auth,
+  errors: state.errors
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-	loginUser
+  loginUser
 }, dispatch);
 
 
 Login.propTypes = {
-	loginUser: propTypes.func.isRequired,
-	auth: propTypes.object.isRequired,
-	errors: propTypes.object.isRequired
+  loginUser: propTypes.func.isRequired,
+  auth: propTypes.object.isRequired,
+  errors: propTypes.object.isRequired
 };
 
 
