a196962eb931eb3c0995b005fce926100ad9351a
nicodev-git <schaefernicker@gmail.com>
2020-07-24 00:50:44 +0800

    change to live url

--- src/client/constants/auth.js
+++ src/client/constants/auth.js
@@ -2,7 +2,7 @@ import { getURLWithQueryParams } from "../utils/generic"
 
 export const LINKEDIN_STATE = '123456'
 const LINKEDIN_SCOPE = 'r_liteprofile r_emailaddress w_member_social'
-const LINKEDIN_RIDERECT = 'http://localhost:3000/callback'
+const LINKEDIN_RIDERECT = (process.env.NODE_ENV === 'production') ? 'https://calm-reaches-80961.herokuapp.com/callback' : 'http://localhost:3000/callback';
 const LINKEDIN_CLIENT_ID = '8610tyv63vt9tn'
 export const LINKEDIN_URL = getURLWithQueryParams('https://www.linkedin.com/oauth/v2/authorization', {
   response_type: "code",
